ab07f5aee3a66598e6e3b3316a4e2a98cba4a9e7
Isaac-Goff <158597448+Isaac-Goff@users.noreply.github.com>
2024-03-27 18:52:34 -0500

    Update main.py
    Got a basic button working to select a random meme from the file we have

--- Code/main.py
+++ Code/main.py
@@ -1,9 +1,36 @@
+import streamlit as st
 import random
 import os
 
-basePath = os.path.basename('')
-allFiles = os.listdir(basePath)
-num = random.randint(1,100)
+"""
+NOTE: To run this you need to get into your python terminal and run the command:
+streamlit run "filename.py"
+At least, my Pycharm required me to do this
+"""
 
-print(basePath)
+def loadmemes():
+    direc = 'Memes for Meme God/'
+    memeList = []
+    for meme in os.listdir(direc):
+        filePath = direc + meme # Directory attached to filename for easy getting
+        memeList.append(filePath)
+    return memeList
 
+
+def getmeme(memeList):
+    meme = memeList[random.randint(0, len(memeList) - 1)]
+    st.subheader('Your meme, your magesty')
+    st.image(meme)
+
+
+
+
+def main():
+    memeList = loadmemes()
+    st.button('Get Meme', type="primary")
+    if st.button:
+        getmeme(memeList)
+
+
+if __name__ == "__main__":
+    main()
