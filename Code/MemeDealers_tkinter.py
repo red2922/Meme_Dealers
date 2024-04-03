bc596ab89c8f93a6a4060e0b56542659e523baae
Isaac-Goff <158597448+Isaac-Goff@users.noreply.github.com>
2024-04-03 10:35:37 -0500

    Update MemeDealers_tkinter.py

--- Code/MemeDealers_tkinter.py
+++ Code/MemeDealers_tkinter.py
@@ -19,14 +19,33 @@ def resize_img(img):
     height = int(float(img.size[1]) * float(width_rate))
     return img.resize((width, height))
 
+'''
 def get_random_meme(memeList, label):
     meme_path = memeList[random.randint(0, len(memeList) - 1)]
     meme_img = Image.open(meme_path)
-    meme = ImageTk.PhotoImage(resize_img(meme_img))
-    label.config(image=meme)
-    label.image = meme  # Keep a reference to the image object
+    meme_resized = ImageTk.PhotoImage(resize_img(meme_img))
+    label.config(image=meme_resized)
+    label.image = meme_resized  # Keep a reference to the image object
+'''
+
+def get_random_meme(memeList, seenList, label):
+    meme = memeList.pop(0)
+    meme_img = Image.open(meme)
+    meme_resized = ImageTk.PhotoImage(resize_img(meme_img))
+    label.config(image=meme_resized)
+    label.image = meme_resized  # Keep a reference to the image object
+    print('forward', meme)
+    seenList.insert(0, meme)
 
 
+def last_meme(memeList, seenList, label):
+    meme = seenList.pop(0)
+    meme_img = Image.open(meme)
+    meme_resized = ImageTk.PhotoImage(resize_img(meme_img))
+    label.config(image=meme_resized)
+    label.image = meme_resized
+    print('backward', meme)
+    memeList.insert(0, meme)
 og = True  # initiate toggle state
 
 
@@ -51,8 +70,13 @@ if __name__ == "__main__":
     image_label.pack()
 
     memes = load_memes()
-    btn = Button(window, text='Get Meme', command=lambda: get_random_meme(memes, image_label))
+    seen = []
+    btn = Button(window, text='Get Meme', command=lambda: get_random_meme(
+        memes, seen, image_label))
+    backbtn = Button(window, text='Last Meme', command=lambda: last_meme(
+        memes, seen, image_label))
     btn.pack()
+    backbtn.pack()
 
     toggle_og_face = ImageTk.PhotoImage(Image.open("toggle_og.jpg"))
     toggle_ai_face = ImageTk.PhotoImage(Image.open("toggle_ai.jpg"))
