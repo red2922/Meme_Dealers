a42dd1fbaf905eb28ef90a8fe84b8fc38e731395
mitchdeer <147088279+mitchdeer@users.noreply.github.com>
2024-04-02 23:21:57 -0500

    Added bare-bones toggle button

--- Code/MemeDealers_tkinter.py
+++ Code/MemeDealers_tkinter.py
@@ -27,6 +27,19 @@ def get_random_meme(memeList, label):
     label.image = meme  # Keep a reference to the image object
 
 
+og = True  # initiate toggle state
+
+
+def toggle():
+    global og
+    if og:
+        toggle_btn.config(image=toggle_ai_face)
+        og = False
+    else:
+        toggle_btn.config(image=toggle_og_face)
+        og = True
+
+
 if __name__ == "__main__":
     window = Tk()
     window.title('Meme Dealers')
@@ -40,5 +53,10 @@ if __name__ == "__main__":
     memes = load_memes()
     btn = Button(window, text='Get Meme', command=lambda: get_random_meme(memes, image_label))
     btn.pack()
+
+    toggle_og_face = ImageTk.PhotoImage(Image.open("toggle_og.jpg"))
+    toggle_ai_face = ImageTk.PhotoImage(Image.open("toggle_ai.jpg"))
+    toggle_btn = Button(window, image=toggle_og_face, command=lambda: toggle())
+    toggle_btn.pack()
 
     window.mainloop()
